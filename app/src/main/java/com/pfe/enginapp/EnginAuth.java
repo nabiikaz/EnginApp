fa01af2a8fa5a28a26fa612ea2b6fabedac311f7
Nabi Zakaria <nabi.zakaria.developer@gmail.com>
2020-05-14 11:01:56 +0100

    EnginAuth design implemented

--- app/src/main/java/com/pfe/enginapp/EnginAuth.java
+++ app/src/main/java/com/pfe/enginapp/EnginAuth.java
@@ -2,13 +2,47 @@ package com.pfe.enginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class EnginAuth extends AppCompatActivity {
+
+    TextInputLayout matricule;
+
+    Button identification_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_engin_auth);
+
+        matricule = findViewById(R.id.matricule);
+        identification_btn = findViewById(R.id.identification_btn);
+
+
     }
+
+    private void enginIdentification(){
+
+        identification_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String matricule_text = matricule.getEditText().getText().toString();
+
+                //check if it exists in the database then proceed to the next activity
+
+
+                //Intent intent = new Intent(EnginAuth.this,)
+
+
+            }
+        });
+    }
+
+
 }
